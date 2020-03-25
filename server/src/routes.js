a2d4767483aa7100ea9471c659368fc548dd0e8a
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-25 01:01:48 -0300

    adicionando controllers

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,23 +1,17 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const ongController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/ongs', async (req, res)  => {
-    const { name, email, whatsapp, city, uf } = req.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-    await connection('ongs').insert({
-        id,
-        name, 
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
-
-    return res.json();
-}); 
+routes.post('/sessions', SessionController.create);
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
